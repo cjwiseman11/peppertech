6833a03a7ce6945b7abd3da006c7461dde0690a6
Chris Wiseman <cjwiseman11@gmail.com>
2019-03-19 11:16:27 +0000

    Dev (#1)
    * More portfolio work
    
    * More Design
    
    * Removed the nav
    
    * Sexy Nav
    
    * Homepage Complete
    
    * Removed Icons Folder
    
    * Removed no break spaces
    
    * Contact Us started
    
    * OriginalNavReturns
    
    * Nav changes
    
    * Contact design finished
    
    * Mobile view complete
    
    * Contact complete
    
    * Contact Form Work
    
    * IE11 Fixes
    
    * Merge Fix

---
+++ submit.php
@@ -0,0 +1,28 @@
+<?php
+	if(!$_POST) {
+		echo "An error occured. What are you up to?";
+		exit;
+	} else {
+		$to = 'chris@peppertech.co.uk'; 
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];	
+		if(get_magic_quotes_gpc()) { 
+			$message = stripslashes($message); 
+		}
+		$subject = 'Peppertech Portfolio Contact Form: ' . $name ;
+		$msg  = "Peppertech Portfolio Contact Form: $name.\r\n\n";
+		$msg .= "$message\r\n\n";
+		$msg .= "You can contact $name via email, $email.\r\n\n";
+		$msg .= "-------------------------------------------------------------------------------------------\r\n";
+								
+		if(@mail($to, $subject, $msg, "From: $email\r\nReturn-Path: $email\r\n"))
+		{
+			echo "Success";
+		}
+		else
+		{
+			echo "Failed";
+		}
+	}
+?>
